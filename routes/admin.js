5d974613d2f94be4712c3c1ab1e1d7fec0cdc4c5
abhisheksharma226 <avisheksharma2004@gmail.com>
2024-08-05 02:02:31 +0530

    Google tags add

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,23 @@
 const { Router } = require("express");
+const Debtor = require('../models/debtor');
+
 
 const router = Router();
 
 
-router.get('/adminHome' , (req , res) => {
-    res.render('adminHome')
-})
+router.get('/adminHome' , async(req , res) => {
+    try {
+        const totalDebtors = await Debtor.countDocuments();
+        
+        // Render the view and pass the debtor data
+        return res.render('adminHome', {
+          totalDebtors,
+        });
+      } catch (error) {
+        console.error('Error fetching admin data:', error);
+        return res.status(500).send('Error fetching admin data');
+      }
+    });
 
 
 
